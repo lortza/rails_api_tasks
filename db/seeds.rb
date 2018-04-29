ef22b063e77b8f2c9d744e20dfc26ae900833788
anne <richardson.ae@gmail.com>
2018-04-29 10:04:23 -0500

    Adds faker data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 User.destroy_all
 
-user = User.create!({name: "Zorro"})
-user.tasks.create!([
-  {name: "Go to store", complete: true, priority: false},
-  {name: "Build Rails API", complete: false, priority: true},
-  {name: "Build React front end", complete: false, priority: true}
+qty = 10
+boolean_options = [true, false]
 
-])
+qty.times do
+  user = User.create!({name: Faker::Cat.name})
+  qty.times do
+    user.tasks.create!([
+      {name: Faker::Hipster.sentence, complete: boolean_options.sample, priority: boolean_options.sample}
+    ])
+  end
+end
 
-user = User.create!({name: "Alejandro"})
-user.tasks.create!([
-  {name: "Hunt for lizards", complete: true, priority: false},
-  {name: "Mow the lawn", complete: false, priority: false},
-  {name: "Write Readme doc", complete: false, priority: true}
+# user = User.create!({name: "Alejandro"})
+# user.tasks.create!([
+#   {name: "Hunt for lizards", complete: true, priority: false},
+#   {name: "Mow the lawn", complete: false, priority: false},
+#   {name: "Write Readme doc", complete: false, priority: true}
 
-])
+# ])
